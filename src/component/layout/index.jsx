dc23e69f4aed0015f01ed7a79d189a6eff6b5b20
Paban12 <pabanmahato604@gmail.com>
2024-10-22 11:04:23 +0530

    latest

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -34,7 +34,7 @@ function Layout({ bg, overlay, children }) {
     // }
 
     if(url[1] != "/signin"){
-      window.location.href =    'http://dealregal.com:5175/signin';//"http://192.168.0.203:5175/signin" //
+      window.location.href =    'http://fairdealprime.com:5175/signin';//"http://192.168.0.203:5175/signin" //
       
       return false
     }
